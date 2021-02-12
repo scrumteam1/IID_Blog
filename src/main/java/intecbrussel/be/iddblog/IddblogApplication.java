d22fc47a876b23578974416117ef6dac0aaf347e
scrumteam1 <mano@Manos-iMac.fritz.box>
2021-02-12 14:00:46 +0100

    first commit

---
+++ src/main/java/intecbrussel/be/iddblog/IddblogApplication.java
@@ -0,0 +1,13 @@
+package intecbrussel.be.iddblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IddblogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IddblogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/intecbrussel/be/iddblog/IddblogApplicationTests.java
@@ -0,0 +1,13 @@
+package intecbrussel.be.iddblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IddblogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
