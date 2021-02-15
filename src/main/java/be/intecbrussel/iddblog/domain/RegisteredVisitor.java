f40d2c612d23c1550b0283c647d1a2cd95deeea4
akyare <akyare@hotmail.com>
2021-02-15 16:13:59 +0100

    Adding validations for RegisteredVisitor

--- src/main/java/be/intecbrussel/iddblog/domain/RegisteredVisitor.java
+++ src/main/java/be/intecbrussel/iddblog/domain/RegisteredVisitor.java
@@ -1,14 +1,17 @@
 package be.intecbrussel.iddblog.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import be.intecbrussel.iddblog.validation.PasswordConstraintValidator;
+import be.intecbrussel.iddblog.validation.ValidPassword;
+import lombok.*;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.Constraint;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Entity
 @Data
@@ -21,18 +24,30 @@ public class RegisteredVisitor {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    @NotBlank(message = "user name is mandatory.")
+    @Size(min = 3, max = 20)
     private String username;
 
+    @NotBlank(message = "first name is mandatory.")
+    @Size(min = 1, max = 20)
     private String firstName;
 
+    @NotBlank(message = "last name is mandatory.")
+    @Size(min = 1, max = 20)
     private String lastName;
 
+    @NotBlank(message = "email is mandatory.")
+    @Email(message = "email should be valid.")
     private String emailAddress;
 
+    @ValidPassword
     private String password;
 
+    @ValidPassword
     private String confirmPassword;
 
     private String gender;
+
+    private Boolean isWriter;
 
 }
