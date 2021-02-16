98d28d8471209e3e66feaa023f4dd5e93d29e510
akyare <akyare@hotmail.com>
2021-02-16 17:19:01 +0100

    Adding file auto created database sql.

---
+++ intec_database_create.sql
@@ -0,0 +1,3 @@
+create table hibernate_sequence (next_val bigint) engine=InnoDB;
+insert into hibernate_sequence values ( 1 );
+create table registered_visitor (id bigint not null, confirm_password varchar(255) not null, email_address varchar(255), first_name varchar(20), gender varchar(255), is_writer bit, last_name varchar(20), password varchar(255), username varchar(20), primary key (id)) engine=InnoDB;
