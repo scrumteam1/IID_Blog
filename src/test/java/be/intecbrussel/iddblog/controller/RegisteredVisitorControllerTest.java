425b045ab0431eaf2d0c47e25a8bc90a0a26b98e
akyare <akyare@hotmail.com>
2021-02-16 10:27:57 +0100

    Adding validations to the register form.

--- src/test/java/be/intecbrussel/iddblog/controller/RegisteredVisitorControllerTest.java
+++ src/test/java/be/intecbrussel/iddblog/controller/RegisteredVisitorControllerTest.java
@@ -17,6 +17,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+
 @ExtendWith(MockitoExtension.class)
 class RegisteredVisitorControllerTest {
 
@@ -48,9 +49,20 @@ class RegisteredVisitorControllerTest {
 
     @Test
     void save() throws Exception {
-        when(visitorService.saveVisitor(ArgumentMatchers.any())).thenReturn(RegisteredVisitor.builder().id(1L).build());
+        RegisteredVisitor savedVisitor = RegisteredVisitor.builder().firstName("Abdel").lastName("Khy")
+                .username("akhyare").emailAddress("ak@hotmail.com").password("uD45Pj6J*@cH$u")
+                .confirmPassword("uD45Pj6J*@cH$u").gender("Male").isWriter(false)
+                .build();
 
-        mockMvc.perform(post("/registeredvisitor"))
+        when(visitorService.saveVisitor(ArgumentMatchers.any())).thenReturn(savedVisitor);
+
+        mockMvc.perform(post("/registeredvisitor")
+                .param("firstName","Abdel")
+                .param("lastName","Khy")
+                .param("username","akhyare")
+                .param("emailAddress","ak@hotmail.com")
+                .param("password","uD45Pj6J*@cH$u")
+                .param("confirmPassword","uD45Pj6J*@cH$u"))
                 .andExpect(status().is3xxRedirection())
                 .andExpect(view().name("redirect:/index"))
                 .andExpect(model().attributeExists("registeredvisitor"));
