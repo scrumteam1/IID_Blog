c38a243d9d8c7d9a8dac33fa0098833285a217f6
crobbere <53408506+crobbere@users.noreply.github.com>
2021-02-16 18:12:31 +0100

    adding security config and login controller

--- src/main/java/be/intecbrussel/iddblog/controller/LoginController.java
+++ src/main/java/be/intecbrussel/iddblog/controller/LoginController.java
@@ -1,2 +1,23 @@
-package be.intecbrussel.iddblog.controller;public class LoginController {
+package be.intecbrussel.iddblog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.security.RolesAllowed;
+
+@Controller
+public class LoginController {
+
+    @RolesAllowed({"USER", "ADMIN"})
+    @RequestMapping("/index")
+    public String getUser(){
+        return "welcome to the user page.";
+    }
+
+    @RolesAllowed("ADMIN")
+    @RequestMapping("/admin")
+    public String getAdmin(){
+        return "admin page.";
+    }
+
 }
---
+++ src/main/java/be/intecbrussel/iddblog/security/SecurityConfig.java
@@ -0,0 +1,40 @@
+package be.intecbrussel.iddblog.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(final AuthenticationManagerBuilder amb) throws Exception {
+
+        amb.inMemoryAuthentication()
+           .withUser("user").password(passwordEncoder().encode("user123")).roles("USER")
+           .and()
+           .withUser("admin").password(passwordEncoder().encode("admin123")).roles("ADMIN", "USER");
+
+    }
+
+    @Override
+    protected void configure (final HttpSecurity httpSecurity) throws Exception{
+        httpSecurity.authorizeRequests()
+                    .antMatchers("/admin").hasRole("ADMIN")
+                    .antMatchers("/user").hasRole("USER")
+                    .anyRequest().authenticated()
+                    .and()
+                    .formLogin();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
