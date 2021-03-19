3d78e6302e2d2bf7b7bed808afdeb6e61bce461b
akyare <akyare@hotmail.com>
2021-03-19 21:48:27 +0100

    Fixing delete admin. Cannot be deleted.

--- src/main/java/be/intecbrussel/iddblog/repository/TagRepository.java
+++ src/main/java/be/intecbrussel/iddblog/repository/TagRepository.java
@@ -1,6 +1,7 @@
 package be.intecbrussel.iddblog.repository;
 
 import be.intecbrussel.iddblog.domain.Tag;
+import be.intecbrussel.iddblog.domain.Tagname;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
@@ -10,4 +11,6 @@ import org.springframework.transaction.annotation.Transactional;
 public interface TagRepository extends CrudRepository<Tag, Long> {
 
     Tag save(Tag tag);
+
+    Tag findByTag(Tagname tagname);
 }
