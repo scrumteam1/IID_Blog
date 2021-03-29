d6241e4a3d408740983bfc4c79e79879997fbba6
akyare <akyare@hotmail.com>
2021-03-29 18:02:30 +0200

    Adding datas and tagnames

--- src/main/scripts/create-schema-mysql.sql
+++ src/main/scripts/create-schema-mysql.sql
@@ -1,59 +1,93 @@
-create table hibernate_sequence
+use blog_dev;
+
+drop table if exists user_authority;
+drop table if exists writerposts_tags;
+drop table if exists authorities;
+drop table if exists hibernate_sequence;
+drop table if exists tags;
+drop table if exists verification_token;
+drop table if exists writerposts;
+drop table if exists users;
+
+
+create table if not exists authorities
+(
+    id bigint not null
+        primary key,
+    authority varchar(255) null
+);
+
+create table if not exists hibernate_sequence
 (
     next_val bigint null
 );
 
-
-create table users
+create table if not exists tags
 (
     id bigint not null
         primary key,
+    tag varchar(255) null
+);
+
+create table if not exists users
+(
+    id bigint not null
+        primary key,
+    avatar longtext null,
     email_address varchar(255) null,
+    enabled bit null,
+    password varchar(255) null,
     first_name varchar(20) null,
     gender varchar(255) null,
     is_writer bit null,
     last_name varchar(20) null,
-    password varchar(255) null,
-    username varchar(20) null,
-    enabled bit default b'0' not null,
-    avatar longtext null,
-    constraint users_username_uindex
-        unique (username)
+    username varchar(20) null
 );
 
-create table authorities
+create table if not exists user_authority
 (
-    id bigint auto_increment
-        primary key,
-    authority varchar(255) null,
-    username varchar(255) null,
-    constraint ix_auth_username
-        foreign key (username) references users (username)
+    user_id bigint not null,
+    auth_id bigint not null,
+    constraint FKhi46vu7680y1hwvmnnuh4cybx
+        foreign key (user_id) references users (id),
+    constraint FKlw8vjvnr05ipx4u5c6m3ypxoq
+        foreign key (auth_id) references authorities (id)
 );
 
-create table verification_token
+create table if not exists verification_token
 (
     id bigint not null
         primary key,
     expiry_date datetime null,
     token varchar(255) null,
     user_id bigint null,
-    constraint fk_veriftoken_users
+    constraint FK3asw9wnv76uxu3kr1ekq4i1ld
         foreign key (user_id) references users (id)
 );
 
 create table if not exists writerposts
 (
-    id            bigint auto_increment
+    id bigint not null
         primary key,
-    creation_date datetime     null,
-    title         varchar(255) null,
-    intro         longtext     not null,
-    content       longtext     null,
-    status        varchar(255) null,
-    user_id       bigint       null,
-    constraint fk_post_users
+    content longtext null,
+    creation_date datetime not null,
+    intro longtext null,
+    is_enabled bit null,
+    status varchar(255) null,
+    title varchar(255) null,
+    user_id bigint null,
+    constraint FK26ksub4ukoimr66c5fu07cmx3
         foreign key (user_id) references users (id)
 );
 
+create table if not exists writerposts_tags
+(
+    post_id bigint not null,
+    tag_id bigint not null,
+    primary key (post_id, tag_id),
+    constraint FKlnj5ewrdwsvpky5njr1lft4b7
+        foreign key (tag_id) references tags (id),
+    constraint FKsim27vq1q63re0qudldn23hu
+        foreign key (post_id) references writerposts (id)
+);
 
